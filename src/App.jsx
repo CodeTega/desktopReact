f2905b19a768a3755654111a46dde26fec29aea0
techbret <bret@techbret.com>
2024-07-05 16:36:40 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen max-h-full bg-zinc-950">
+      <div className="font-bold text-white">Testing</div>
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+const root = createRoot(document.body);
+root.render(<App />);
